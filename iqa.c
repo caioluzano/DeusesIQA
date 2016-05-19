132f786d6064698ff37d04dfa509e0b6184a4fb9
Daniel Moniz <macbookpro@MacBook-Pro--Aipi.local>
2016-05-18 23:28:14 -0300

    alterado

--- iqa.c
+++ iqa.c
@@ -8,9 +8,10 @@ double calcColiformes(double CF)
 	double const B = -36.45;
 	double const C = 3.138;
 	double const D = 0.06776;
+	double q1;
 	
 	if (CF<pow(10,5)){
-		double q1 = A + (B * log(CF)) + (C * log(pow(CF,2))) + (D * log(pow(CF, 3)));
+		q1 = A + (B * log(CF)) + (C * log(pow(CF,2))) + (D * log(pow(CF, 3)));
 	}else{
 		q1 = 3.0;
 
