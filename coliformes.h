b21dec952ea3656d85d0725f3cd13cff576f3bbb
= <japa1996@hotmail.com>
2016-06-07 23:55:05 -0300

    Atualizao do coco

--- coliformes.h
+++ coliformes.h
@@ -1,16 +1,11 @@
-double calcColiformes(double CF)
-{
-	double const A = 98.03; 
-	double const B = -36.45;
-	double const C = 3.138;
-	double const D = 0.06776;
+double calcColiformes(double CF){
 	double q1;
-	
-	if (CF<pow(10,5)){
-		q1 = A + (B * log(CF)) + (C * log(pow(CF,2))) + (D * log(pow(CF, 3)));
-	}else{
-		q1 = 3.0;
 
+	if(CF <= pow(10, 5)){
+		q1 = 98.24034 - 34.7145 * (log10(CF)) + 2.614267 * pow((log10(CF)), 2) + 0.107821 * pow(log10(CF), 3);
+	}
+	if(CF > pow(10, 5)){
+		q1 = 3;
 	}
 	
 	return q1;
